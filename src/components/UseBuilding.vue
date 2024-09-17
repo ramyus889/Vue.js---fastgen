09e74b203a63a1d7b4244af026752731ed006e0e
ramyus889 <ramyus889.com>
2024-09-17 11:34:51 +0300

    fix

--- src/components/UseBuilding.vue
+++ src/components/UseBuilding.vue
@@ -15,14 +15,14 @@
       <div class="flex place-content-center">
         <div class="flex md:flex-row flex-col gap-5 items-center">
           <button
-            data-aos="fade-right"
+            data-aos="fade-up"
             data-aos-delay="500"
             class="group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur hover:underline hover:underline-offset-4 origin-left hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 h-12 w-52 border text-left p-3 text-gray-50 text-base font-bold rounded-full overflow-hidden before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-violet-500 before:rounded-full before:blur-lg after:absolute after:z-10 after:w-20 after:h-20 after:content[''] after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg"
           >
             Get Started
           </button>
           <a
-            data-aos="fade-left"
+            data-aos="fade-up"
             data-aos-delay="500"
             href="#"
             class="relative duration-500 group cursor-pointer text-sky-50 overflow-hidden h-12 w-52 rounded-2xl bg-sky-800 p-2 flex justify-center items-center font-extrabold"
