3c3799e3c6a10cd9888d3ab2fc422d13b89e13cc
ramyus889 <ramyus889.com>
2025-03-10 13:54:19 +0300

    Добавить поведение прокрутки для маршрутизатора

--- src/main.js
+++ src/main.js
@@ -24,7 +24,14 @@ app.use(PrimeVue, {
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  }
 });
 
 app.component('UseTabs', UseTabs);
